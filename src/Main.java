a6f446a59196ded14b053feafe723f70b05b43a6
Zhanserik <zhanserik_email@gmail.com>
2026-09-09 14:02:01 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Аты-жөнім: Қолдасын Әбілқасым");
+        System.out.println("Тобым: 3-MCS");
+        System.out.println("Мен Java бағдарламасын JVM арқылы іске қосып отырмын");
+    }
+}
