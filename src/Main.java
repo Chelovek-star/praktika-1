23571b6e85f1f0624463849c2a429d4478910f66
Zhanserik <zhanserik_email@gmail.com>
2026-09-09 17:47:33 +0500

    Improve console output

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Аты-жөнім: Қолдасын Әбілқасым");
         System.out.println("Тобым: 3-MCS");
         System.out.println("Мен Java бағдарламасын JVM арқылы іске қосып отырмын");
+        System.out.println("Практикалық жұмыс аяқталды");
     }
-}
+}git
